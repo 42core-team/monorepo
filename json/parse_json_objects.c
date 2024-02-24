6a70ed7f64a1bcfc949d721b5245e3ec19e404db
pgrossma <pgrossma@student.42heilbronn.de>
2024-02-24 16:55:18 +0000

    started adding resource config

--- json/parse_json_objects.c
+++ json/parse_json_objects.c
@@ -205,3 +205,35 @@ t_unit_config	*ft_parse_unit_config(int token_ind, int token_len, jsmntok_t *tok
 
 	return (unit_configs);
 }
+
+t_resource_config	*ft_parse_resource_config(int token_ind, int token_len, jsmntok_t *tokens, char *json)
+{
+	t_resource_config	*resource_configs;
+	int					last_json_index, next_token_ind;
+
+	token_ind = ft_find_token_one("resources", token_ind, token_len, tokens, json);
+	if (token_ind == -1)
+		return (NULL);
+	last_json_index = tokens[token_ind].end;
+
+	resource_configs = malloc(sizeof(t_resource_config));
+	resource_configs[0].type_id = 0;
+
+	int index = 0;
+	while (token_ind != -1)
+	{
+		next_token_ind = ft_find_token_one("type_id", token_ind, token_len, tokens, json);
+		if (next_token_ind == -1 || tokens[next_token_ind].end > last_json_index)
+			break;
+
+		resource_configs = realloc(resource_configs, sizeof(t_resource_config) * (index + 2));
+		resource_configs[index + 1].type_id = 0;
+
+		resource_configs[index].type_id = ft_find_parse_ulong("type_id", &token_ind, token_len, tokens, json);
+		resource_configs[index].hp = ft_find_parse_ulong("hp", &token_ind, token_len, tokens, json);
+
+		index++;
+	}
+
+	return (resource_configs);
+}
