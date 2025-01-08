d937bac17a6819f5c8d2e4562ea65987acc4102b
FreddyMSchubert <github@frederickschubert.de>
2025-01-08 08:47:24 +0000

    Mkae ft_travel_attack better (units no longer stop at edge of range, thus continuously leaving range if walking slower)

--- actions.c
+++ actions.c
@@ -150,14 +150,16 @@ void	ft_travel_attack(t_obj *attacker_unit, t_obj *attack_obj)
 	if (!attacker_unit || !attack_obj || attacker_unit->state != STATE_ALIVE || attack_obj->state != STATE_ALIVE)
 		return ;
 
-	double dist = ft_distance(attacker_unit, attack_obj);
 	t_unit_config *attacker_config = ft_get_unit_config(attacker_unit->s_unit.type_id);
 	if (!attacker_config)
 		ft_attack(attacker_unit, attack_obj);
 
-	if (dist > attacker_config->max_range)
+	double dist = ft_distance(attacker_unit, attack_obj);
+	double intendedDist = (attacker_config->max_range + attacker_config->min_range) / 2;
+
+	if (dist > intendedDist)
 		ft_travel_to_obj(attacker_unit, attack_obj);
-	else if (dist < attacker_config->min_range)
+	else if (dist < intendedDist)
 		ft_travel_to_obj(attacker_unit, ft_get_my_core());
 	else
 		ft_travel_to_obj(attacker_unit, attacker_unit);
