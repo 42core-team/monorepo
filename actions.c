9d2ef0d9f7b7a603ca38bd774b7f35e83a7efe57
Freddy <120249199+FreddyMSchubert@users.noreply.github.com>
2025-01-21 21:27:48 +0100

    ft_travel_attack simplification

--- actions.c
+++ actions.c
@@ -155,22 +155,6 @@ void	ft_attack(t_obj *attacker_unit, t_obj *target_obj)
 
 void	ft_travel_attack(t_obj *attacker_unit, t_obj *attack_obj)
 {
-	if (!attacker_unit || !attack_obj || attacker_unit->state != STATE_ALIVE || attack_obj->state != STATE_ALIVE)
-		return ;
-
-	t_unit_config *attacker_config = ft_get_unit_config(attacker_unit->s_unit.type_id);
-	if (!attacker_config)
-		ft_attack(attacker_unit, attack_obj);
-
-	double dist = ft_distance(attacker_unit, attack_obj);
-	double intendedDist = (attacker_config->max_range + attacker_config->min_range) / 2;
-
-	if (dist > intendedDist)
-		ft_travel_to_obj(attacker_unit, attack_obj);
-	else if (dist < intendedDist)
-		ft_travel_to_obj(attacker_unit, ft_get_my_core());
-	else
-		ft_travel_to_obj(attacker_unit, attacker_unit);
-
+	ft_travel_to_obj(attacker_unit, attack_obj);
 	ft_attack(attacker_unit, attack_obj);
 }
