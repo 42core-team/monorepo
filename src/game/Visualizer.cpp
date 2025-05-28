a6f6a1364aa443010954d80bcf8d62a73e68be1d
FreddyMSchubert <github@frederickschubert.de>
2025-05-28 09:03:55 +0200

    Visualizer emoji utilization

--- src/game/Visualizer.cpp
+++ src/game/Visualizer.cpp
@@ -1,63 +1,96 @@
 #include "Game.h"
-
+#include <iostream>
 #include <string>
+#include <unordered_map>
+#include <vector>
 
 void Game::visualizeGameState(unsigned long long tick)
 {
-	std::cout << "Tick: " << tick << "; Core: C / c; Units: W / w = Warrior; M / m = Miner; . = empty; R = Resource; X = Wall" << std::endl;
+	std::cout << "Tick: " << tick << std::endl;
 
-	for (int y = 0; y < (int)Config::getInstance().height; y++)
+	std::cout << "Cores: " << "🏰 🏯" << std::endl;
+	static const std::pair<std::string, std::string> CORE_EMOJI = {u8"🏰", u8"🏯"};
+
+	std::cout << "Units: "
+			  << "Warrior: 🤾‍♂️/🤾; Miner: 🏋️‍♂️/🏋️; Carrier: 🤹‍♂️/🤹; "
+				 "Builder: 👷‍♂️/👷; Bomberman: 👨‍🎤/🧑‍🎤"
+			  << std::endl;
+	static const std::vector<std::pair<std::string, std::string>> UNIT_EMOJI = {
+		{u8"🤾‍♂️", u8"🤾"},
+		{u8"🏋️‍♂️", u8"🏋️"},
+		{u8"🤹‍♂️", u8"🤹"},
+		{u8"👷‍♂️", u8"👷"},
+		{u8"👨‍🎤", u8"🧑‍🎤"}};
+
+	std::cout << "Objects: " << "Resource: 💎; Wall: 🧱; Money: 💰; Bomb: 💣" << std::endl;
+	static const std::unordered_map<ObjectType, std::string> SINGLE_EMOJI = {
+		{ObjectType::Resource, u8"💎"},
+		{ObjectType::Wall, u8"🧱"},
+		{ObjectType::Money, u8"💰"},
+		{ObjectType::Bomb, u8"💣"}};
+
+	static const std::string EMPTY_CELL = u8"\u3000";
+
+	int H = Config::getInstance().height;
+	int W = Config::getInstance().width;
+
+	std::cout << u8"╔";
+	for (int x = 0; x < W; ++x)
+		std::cout << u8"═" << u8"═";
+	std::cout << u8"╗\n";
+
+	for (int y = 0; y < H; ++y)
 	{
-		for (int x = 0; x < (int)Config::getInstance().width; x++)
+		std::cout << u8"║";
+		for (int x = 0; x < W; ++x)
 		{
-			Position pos = Position(x, y);
-			Object * obj = getObjectAtPos(pos);
-			if (obj)
+			auto pos = Position(x, y);
+			Object *obj = getObjectAtPos(pos);
+			if (!obj)
 			{
-				if (obj->getType() == ObjectType::Core)
-				{
-					Core * core = dynamic_cast<Core*>(obj);
-					std::cout << "Cc"[core->getTeamId() % 2];
-				}
-				else if (obj->getType() == ObjectType::Unit)
-				{
-					Unit * unit = dynamic_cast<Unit*>(obj);
-					std::string unitInd = "??";
-					if (unit->getTypeId() == 0)
-						unitInd = "Ww";
-					else if (unit->getTypeId() == 1)
-						unitInd = "Mm";
-					else if (unit->getTypeId() == 2)
-						unitInd = "Cc";
-					else if (unit->getTypeId() == 3)
-						unitInd = "Bb";
-					else if (unit->getTypeId() == 4)
-						unitInd = "Aa";
-					std::cout << unitInd[unit->getTeamId() % 2];
-				}
-				else if (obj->getType() == ObjectType::Resource)
-				{
-					std::cout << "R";
-				}
-				else if (obj->getType() == ObjectType::Wall)
-				{
-					std::cout << "X";
-				}
-				else if (obj->getType() == ObjectType::Money)
-				{
-					std::cout << "$";
-				}
-				else if (obj->getType() == ObjectType::Bomb)
-				{
-					std::cout << "B";
-				}
+				std::cout << EMPTY_CELL;
+				continue;
 			}
-			else
+
+			switch (obj->getType())
 			{
-				std::cout << ".";
+			case ObjectType::Core:
+			{
+				Core *core = dynamic_cast<Core *>(obj);
+				int team = core->getTeamId() % 2;
+				std::cout << (team == 0 ? CORE_EMOJI.first : CORE_EMOJI.second);
+				break;
+			}
+
+			case ObjectType::Unit:
+			{
+				Unit *unit = dynamic_cast<Unit *>(obj);
+				int tid = unit->getTypeId();
+				int team = unit->getTeamId() % 2;
+				if (tid >= 0 && tid < (int)UNIT_EMOJI.size())
+				{
+					auto &tones = UNIT_EMOJI[tid];
+					std::cout << (team == 0 ? tones.first : tones.second);
+				}
+				else
+				{
+					std::cout << u8"❓";
+				}
+				break;
+			}
+
+			default:
+			{
+				auto it = SINGLE_EMOJI.find(obj->getType());
+				std::cout << (it != SINGLE_EMOJI.end() ? it->second : u8"❔");
+			}
 			}
 		}
-		std::cout << std::endl;
+		std::cout << u8"║\n";
 	}
-	std::cout << std::endl;
+
+	std::cout << u8"╚";
+	for (int x = 0; x < W; ++x)
+		std::cout << u8"═" << u8"═";
+	std::cout << u8"╝\n\n";
 }
