3d749346e50f40567776ce83f0020806bf7348f7
FreddyMSchubert <github@frederickschubert.de>
2025-06-13 23:12:55 +0000

    incorrect json key segfault fix

--- src/json/parse_json_config.c
+++ src/json/parse_json_config.c
@@ -31,7 +31,7 @@ static t_unit_config	**ft_parse_unit_config(json_node * root)
 		units[i]->dmg_unit = (long)json_find(unit_node, "damageUnit")->number;
 		units[i]->dmg_resource = (long)json_find(unit_node, "damageResource")->number;
 		units[i]->dmg_wall = (long)json_find(unit_node, "damageWall")->number;
-		units[i]->dmg_bomb = (unsigned long)json_find(unit_node, "attackRange")->number;
+		units[i]->dmg_bomb = (unsigned long)json_find(unit_node, "damageBomb")->number;
 		units[i]->attack_type = (t_attack_type)json_find(unit_node, "attackType")->number;
 		units[i]->can_build = json_find(unit_node, "canBuild")->number;
 	}
