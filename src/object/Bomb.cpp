04850bf74bb9b49ad43f5e17e2f2b4e1eee3e628
FreddyMSchubert <github@frederickschubert.de>
2025-06-14 14:57:59 +0200

    bomb attack logic stop at walls

--- src/object/Bomb.cpp
+++ src/object/Bomb.cpp
@@ -5,32 +5,55 @@
 Bomb::Bomb(unsigned int id, Position pos)
 	: Object(id, pos, Config::getInstance().bombHp, ObjectType::Bomb), countdown_(Config::getInstance().bombCountdown) {}
 
-void Bomb::tick(unsigned long long tickCount, Game * game)
+void Bomb::tick(unsigned long long tickCount, Game *game)
 {
-	(void) tickCount;
+	(void)tickCount;
 	countdown_--;
 
-	if (countdown_ == 0) {
+	if (countdown_ == 0)
+	{
 		int bombReach = Config::getInstance().bombReach;
-		for (int i = -bombReach; i <= bombReach; i++) {
-			Position pos = Position(position_.x + i, position_.y);
-			Object * obj = game->getObjectAtPos(pos);
-			if (obj) {
-				obj->setHP(obj->getHP() - Config::getInstance().bombDamage);
-			}
+		for (int i = 0; i < bombReach; i++) // pos X
+		{
+			Object *obj = game->getObjectAtPos(Position(position_.x + i, position_.y));
+			if (!obj)
+				continue;
+			if (obj->getType() == ObjectType::Wall)
+				break;
+			obj->setHP(obj->getHP() - Config::getInstance().bombDamage);
 		}
-		for (int i = -bombReach; i <= bombReach; i++) {
-			Position pos = Position(position_.x, position_.y + i);
-			Object * obj = game->getObjectAtPos(pos);
-			if (obj) {
-				obj->setHP(obj->getHP() - Config::getInstance().bombDamage);
-			}
+		for (int i = 0; i < bombReach; i++) // pos Y
+		{
+			Object *obj = game->getObjectAtPos(Position(position_.x, position_.y + i));
+			if (!obj)
+				continue;
+			if (obj->getType() == ObjectType::Wall)
+				break;
+			obj->setHP(obj->getHP() - Config::getInstance().bombDamage);
+		}
+		for (int i = 0; i < bombReach; i++) // neg X
+		{
+			Object *obj = game->getObjectAtPos(Position(position_.x - i, position_.y));
+			if (!obj)
+				continue;
+			if (obj->getType() == ObjectType::Wall)
+				break;
+			obj->setHP(obj->getHP() - Config::getInstance().bombDamage);
+		}
+		for (int i = 0; i < bombReach; i++) // neg Y
+		{
+			Object *obj = game->getObjectAtPos(Position(position_.x, position_.y - i));
+			if (!obj)
+				continue;
+			if (obj->getType() == ObjectType::Wall)
+				break;
+			obj->setHP(obj->getHP() - Config::getInstance().bombDamage);
 		}
 		game->removeObjectById(this->getId());
 	}
 }
 
-std::unique_ptr<Object> & Bomb::clone(Position newPos, Game * game) const
+std::unique_ptr<Object> &Bomb::clone(Position newPos, Game *game) const
 {
 	int nextObjId = game->getNextObjectId();
 	std::unique_ptr<Object> obj = std::make_unique<Bomb>(nextObjId, newPos);
