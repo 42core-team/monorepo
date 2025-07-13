6dcdf5ba5c2e19568f89a37a5af1afdec0ca54ce
FreddyMSchubert <github@frederickschubert.de>
2025-07-13 07:35:35 +0000

    distance util variable naming improvements

--- src/internal/utils.c
+++ src/internal/utils.c
@@ -2,13 +2,13 @@
 
 int core_internal_util_distance(t_pos pos1, t_pos pos2)
 {
-	double x = (double)pos1.x - (double)pos2.x;
-	double y = (double)pos1.y - (double)pos2.y;
+	double deltaX = (double)pos1.x - (double)pos2.x;
+	double deltaY = (double)pos1.y - (double)pos2.y;
 
-	if (x < 0)
-		x = -x;
-	if (y < 0)
-		y = -y;
+	if (deltaX < 0)
+		deltaX = -deltaX;
+	if (deltaY < 0)
+		deltaY = -deltaY;
 
-	return ((int)(x + y));
+	return ((int)(deltaX + deltaY));
 }
