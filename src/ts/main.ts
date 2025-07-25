de8ec998fac1d2787a17f6b27b7cf57ecaccc2a7
FreddyMSchubert <github@frederickschubert.de>
2025-07-25 22:12:48 +0200

    live-reloading replay test

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,13 @@
+import { getStateAt, loadReplay } from './replay_loader/replayLoader.js';
+
 console.log('Debug Visualizer is running!');
+
+setInterval(async () => {
+	await loadReplay('../misc/replay.json');
+	const state = getStateAt(0);
+	if (state) {
+		console.log('State at tick 0:', state);
+	} else {
+		console.log('No state found at tick 0');
+	}
+}, 1000);
