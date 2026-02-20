cbda85df240486e62e7fe3a8d6084a79d2dbbe85
Jonas Kauker <jonas.kauker2003@gmail.com>
2026-02-20 15:32:59 +0100

    simple go bot repo template

---
+++ bots/go/template/my-core-bot/src/go.mod
@@ -0,0 +1,5 @@
+module coregamegobot
+
+go 1.25.0
+
+require github.com/Reptudn/goConn v0.0.3 // indirect
---
+++ bots/go/template/my-core-bot/src/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+
+	goconn "github.com/Reptudn/goConn"
+	"github.com/Reptudn/goConn/shared"
+)
+
+const teamName = "GoConnBotExample"
+
+var coreBot *goconn.CoreGameBot
+
+func tick(game *shared.Game) {
+	game.Log("Tick %d", game.ElapsedTicks)
+
+	// Create a unit if we have enough gems
+	_ = coreBot.CreateUnit(shared.UnitWarrior)
+
+	// Move all our units
+	for _, obj := range game.GetTeamUnits() {
+		game.Log("Unit %d at position (%d, %d)", obj.Id, obj.Pos.X, obj.Pos.Y)
+		_ = coreBot.Move(obj, shared.NewPosition(obj.Pos.X+1, obj.Pos.Y))
+	}
+}
+
+func main() {
+	cgb, err := goconn.NewCoreGameBot(teamName)
+	if err != nil {
+		fmt.Printf("Error creating bot: %v\n", err)
+		return
+	}
+	coreBot = cgb
+
+	if err := coreBot.Run(tick); err != nil {
+		fmt.Printf("Bot error: %v\n", err)
+	}
+}
