61d2150369258080791e8da61bec0b499495c602
FreddyMSchubert <github@frederickschubert.de>
2026-02-21 23:37:05 +0000

    further fix addObjectInfo doxygen documentation

--- bots/c/client_lib/inc/core_lib.h
+++ bots/c/client_lib/inc/core_lib.h
@@ -272,7 +272,7 @@ t_unit_config *core_get_unitConfig(t_unit_type type);
 // ----- DEBUG FUNCTIONS -----
 
 /// @brief Add debug information to an object for visualization
-/// @details This function accumulates debug strings for objects. Multiple calls per tick will append the strings (with newlines).
+/// @details This function accumulates debug strings for objects. Multiple calls per tick will append the strings.
 /// @param obj The object to attach debug info to
 /// @param format printf-style format debug string to add
 /// @param ... Format arguments matching the format string
