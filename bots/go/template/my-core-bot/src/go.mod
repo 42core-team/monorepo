efe886a0fab1597fe9c9cf7ab801525e40f584d0
Jonas Kauker <jonas.kauker2003@gmail.com>
2026-02-24 15:39:26 +0100

    works on my machine (not)

--- bots/go/template/my-core-bot/src/go.mod
+++ bots/go/template/my-core-bot/src/go.mod
@@ -4,3 +4,7 @@ go 1.26.0
 
 require github.com/42core-team/go-client-lib v0.0.0
 
+replace (
+	github.com/42core-team/go-client-lib => ../../../client_lib
+)
+
--- bots/go/template/my-core-bot/src/main.go
+++ bots/go/template/my-core-bot/src/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/42core-team/go-client-lib"
+	"github.com/42core-team/go-client-lib/COREGAME"
 	"github.com/42core-team/go-client-lib/shared"
 )
 
